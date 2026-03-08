7288091c820a82a8e118ed5ed4e50034e577c20b
nalion <nallion.xiaomi@gmail.com>
2026-03-08 23:03:42 +0100

    22aWNDe0v0

--- TdLib.Wrapper/pch.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <collection.h>
-#include <ppltasks.h>
-#include <windows.h>
-#include <string>
-#include <codecvt>
--- TelegramClient/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TelegramClient")]
-[assembly: AssemblyDescription("Telegram для Windows Phone 10")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
