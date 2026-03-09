e82a207cdc21af9b593f7f93bfa43353ab31e7ab
nalion <nallion.xiaomi@gmail.com>
2026-03-09 07:29:34 +0100

    FZInOFCgF1

--- TelegramWP10/ChatItem.cs
+++ TelegramWP10/ChatItem.cs
@@ -1,13 +1,29 @@
 using System;
+using System.ComponentModel;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace TelegramWP10
 {
-    public class ChatItem
+    public class ChatItem : INotifyPropertyChanged
     {
         public long Id { get; set; }
         public string Title { get; set; }
-        // Стандартная заглушка, если фото еще не загружено
-        public BitmapImage Photo { get; set; } = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png"));
+        
+        private BitmapImage _photo = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png"));
+        public BitmapImage Photo 
+        { 
+            get => _photo; 
+            set 
+            { 
+                _photo = value; 
+                OnPropertyChanged("Photo"); 
+            } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
