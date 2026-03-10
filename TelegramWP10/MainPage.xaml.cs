064cc583a687fe461f354cfff9cb7e24c8b21f55
nalion <nallion.xiaomi@gmail.com>
2026-03-10 10:45:54 +0100

    dAsIXifmrR

--- TelegramWP10/MainPage.xaml.cs
+++ TelegramWP10/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
