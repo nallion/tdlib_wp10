b65a6d49afaaa4b757f888cd2d591c4c0f43e287
nalion <nallion.xiaomi@gmail.com>
2026-03-14 00:34:08 +0100

    pVWyfDBslU

--- TelegramWP10/MainPage.xaml.cs
+++ TelegramWP10/MainPage.xaml.cs
@@ -198,8 +198,8 @@ namespace TelegramWP10
 
         private async void InitAsync() {
             try {
-                var localFolder = Windows.Storage.KnownFolders.MusicLibrary;
-                var appFolder = await localFolder.CreateFolderAsync("TelegramWP10", CreationCollisionOption.OpenIfExists);
+                var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                var appFolder = await localFolder.CreateFolderAsync("Unogram", CreationCollisionOption.OpenIfExists);
                 _dbPath = appFolder.Path.Replace("\\", "/") + "/td_db";
                 _filesFolder = await appFolder.CreateFolderAsync("td_db_files", CreationCollisionOption.OpenIfExists);
                 string logName = "log_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
