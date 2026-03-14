e64cb1b1bfddb8faf4295922dd705a375f093a6b
nalion <nallion.xiaomi@gmail.com>
2026-03-14 10:22:35 +0100

    f1PVnjEw20

--- TelegramWP10/MainPage.xaml.cs
+++ TelegramWP10/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.Storage;
+using Windows.Storage.Streams;
 using Newtonsoft.Json.Linq;
 
 namespace TelegramWP10
